fc2a84f271145c214c0c6e87b2c741a3de82ffec
Hellerlight <77800527+Hellerlight@users.noreply.github.com>
2021-12-11 23:22:19 +0200

    Initial commit fix

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+void init(int argc, char *argv[]) {
+}
+
+int main(int argc, char *argv[]) {
+    init(argc, argv);
+    printf("Hello world!\n");
+}
